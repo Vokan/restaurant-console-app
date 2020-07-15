637e0299c8f43a7795fde3b8c8e3a276c9113082
Vokan Kocak <vokankocak@gmail.com>
2020-07-15 09:53:49 +0200

    TP1.2

--- src/main/java/dev/config/AppConfig.java
+++ src/main/java/dev/config/AppConfig.java
@@ -1,8 +1,20 @@
 package dev.config;
 
+import java.util.Scanner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan("dev")
+@PropertySource("app.properties")
 public class AppConfig {
+	
+	@Bean
+	 public Scanner scanner() {
+       return new Scanner(System.in);
+   }
 
 }
